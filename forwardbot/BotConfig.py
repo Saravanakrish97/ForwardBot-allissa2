30bfa80ebd6b5afbdfd9e8ea6bc2c4ddc6001a71
flmorse <69221003+flmorse@users.noreply.github.com>
2021-12-26 12:38:07 +0530

    Update BotConfig.py

--- forwardbot/BotConfig.py
+++ forwardbot/BotConfig.py
@@ -1,10 +1,10 @@
 from os import environ
 class Config(object):
-    API_ID = "12416481"
-    API_HASH = "80f3fa70039ffda055dcf94fe97e75fb"
-    BOT_TOKEN = "5009748966:AAH_7XseUnx8lfWx6e8wcw9dYmmQP6r4D14"
-    STRING_SESSION = "1AZWarzoBu4fVfgme6Jwv06oYdbNH33fZjnpAcsXBqALDEUFochgSOyx4oobacuunDVNRJ9SXNYxOdqiCptsgwNmv2Y7Mqv9AjeX0iYt9inSBZ04bk15EvtMgCpWtdwU0DDSuAN_U1hVbFPvHGNaDxAlf1Eytw6M16nbA7OhIqDzWL9u1B465gMDJPRyWv3vfJVk1HA5iegbklQJ--WYSa86L3q52PrNs15NOGP4f1ueQGVwDdGXwiNRjGe2kOSareN3LwYEJ6Y0hIYgGkTSAMcG023l0UXjxlyOw91lgFFYiRVME-on2vIN_XtRwKo4YI-atrUF5alM8_mpJrtbcryLy6VkPwOg="
-    SUDO_USERS = "5011421350"
+    API_ID = "9821348"
+    API_HASH = "c03b8a9b7724e7acd4635cf9e9a33b10"
+    BOT_TOKEN = "5006516736:AAGhNGMsDKKoIl2hSnxiwsiNNiBl5IFSQnc"
+    STRING_SESSION = "1AZWarzoBuydNVtKKs9YTgKuz2h4SDgq_dn53v2hKoFIjCi8_qZlGz4UvX6xC_ha-PUbWbWxn6lELikEV3QhA-eMPU6MS7hgFPAkxGpU30D3da18TnFfmRsnIydv-0ZPxx-auRNAdv5cQNhrZ2IgaCRhAonHCZuZHVBp5_Xk2iqzfbOqWD9xXS-9ty_oa3m-_NniYJj38akJD65Cypb6E8GdB330XevlaISzmJj4qSe-afZwFUPAXNa6EJun7wHpjsoIAcx4JqPPrmztvISs1QWNZNGblWrcpeRH66DjnIyEdoO5ts_SPyKFx26f8nJwLf5Pb-nsEWnJsi8TMidJGLqrwTuKEf1I="
+    SUDO_USERS = "5051794395"
     COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "^/")
     HELP_MSG = """
     The Commands in the bot are:
