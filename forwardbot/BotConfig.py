1d2bebf8e977ab2692cdf6b86ccc2f7d1e51b4ae
Saravanakrish97 <95554319+Saravanakrish97@users.noreply.github.com>
2022-07-09 22:44:52 +0530

    Update BotConfig.py

--- forwardbot/BotConfig.py
+++ forwardbot/BotConfig.py
@@ -4,7 +4,7 @@ class Config(object):
     API_HASH = "c03b8a9b7724e7acd4635cf9e9a33b10"
     BOT_TOKEN = "5006516736:AAGhNGMsDKKoIl2hSnxiwsiNNiBl5IFSQnc"
     STRING_SESSION = "1AZWarzoBuydNVtKKs9YTgKuz2h4SDgq_dn53v2hKoFIjCi8_qZlGz4UvX6xC_ha-PUbWbWxn6lELikEV3QhA-eMPU6MS7hgFPAkxGpU30D3da18TnFfmRsnIydv-0ZPxx-auRNAdv5cQNhrZ2IgaCRhAonHCZuZHVBp5_Xk2iqzfbOqWD9xXS-9ty_oa3m-_NniYJj38akJD65Cypb6E8GdB330XevlaISzmJj4qSe-afZwFUPAXNa6EJun7wHpjsoIAcx4JqPPrmztvISs1QWNZNGblWrcpeRH66DjnIyEdoO5ts_SPyKFx26f8nJwLf5Pb-nsEWnJsi8TMidJGLqrwTuKEf1I="
-    SUDO_USERS = "5051794395"
+    SUDO_USERS = "1169076058"
     COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "^/")
     HELP_MSG = """
     The Commands in the bot are:
